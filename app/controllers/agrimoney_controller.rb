ecff595000784326b5302e367eba41146dec9b27
quaresma <rodrigoquaresma@gmail.com>
2013-09-11 13:11:41 -0300

    fized

--- app/controllers/agrimoney_controller.rb
+++
@@ -1,9 +0,0 @@
-#coding: utf-8
-class AgrimoneyController < ApplicationController
-    
-  def index
-    @content = params[:content]
-    render 'site/index'
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Agrimoney::Application.routes.draw do
 
-get "/", :to => "agrimoney#index", :as => :agrimoney_index, :defaults => { :content => "home" }
-get "/o-evento", :to => "agrimoney#index", :as => :agrimoney_evento, :defaults => { :content => "evento" }
-get "/programacao", :to => "agrimoney#index", :as => :agrimoney_programacao, :defaults => { :content => "programacao" }
-get "/palestrantes", :to => "agrimoney#index", :as => :agrimoney_palestrantes, :defaults => { :content => "palestrantes" }
-get "/local", :to => "agrimoney#index", :as => :agrimoney_local, :defaults => { :content => "local" }
-get "/inscricoes", :to => "agrimoney#index", :as => :agrimoney_inscricoes, :defaults => { :content => "inscricoes" }
-get "/contato", :to => "agrimoney#index", :as => :agrimoney_contato, :defaults => { :content => "contato" }
-  
+root 'site#index', :as => :agrimoney_home, :defaults => { :content => "home" }
+
+get "/o-evento", :to => "site#index", :as => :agrimoney_evento, :defaults => { :content => "evento" }
+get "/programacao", :to => "site#index", :as => :agrimoney_programacao, :defaults => { :content => "programacao" }
+get "/palestrantes", :to => "site#index", :as => :agrimoney_palestrantes, :defaults => { :content => "palestrantes" }
+get "/local", :to => "site#index", :as => :agrimoney_local, :defaults => { :content => "local" }
+get "/inscricoes", :to => "site#index", :as => :agrimoney_inscricoes, :defaults => { :content => "inscricoes" }
+get "/contato", :to => "site#index", :as => :agrimoney_contato, :defaults => { :content => "contato" }
+
 end
